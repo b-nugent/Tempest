379bb1fa2f31dc3e88f81fd92cfde79493953838
b-nugent <b.nugent@outlook.com>
2017-07-04 19:26:48 -0400

    Added some time utility functions as well as a method for averaging the daily weather data into a monthly data entry.

--- imports/helpers/server/daily-utils.js
+++ imports/helpers/server/daily-utils.js
@@ -1,6 +1,19 @@
 import { DailyAverages } from "/imports/api/daily.js";
 import { MonthlyAverages } from "/imports/api/monthly.js";
+import { getCurrentMonth, getDaysInMonth } from "/imports/helpers/server/time-utils";
 
-export function calculateDailyAverage(currentTime = new Date()) {
+export function calculateDailyAverage() {
+    const daysInCurrentMonth = getDaysInMonth(getCurrentMonth());
+    const dailyAverages = DailyAverages.find({}, {dateCreated: 1}).limit(daysInCurrentMonth).fetch();
 
+    const monthlyAverages = {
+        temperature: _.reduce(_.pluck(dailyAverages, "temperature")) / dailyAverages.length,
+        barometer: _.reduce(_.pluck(dailyAverages, "barometer")) / dailyAverages.length,
+        luxometer: _.reduce(_.pluck(dailyAverages, "luxometer")) / dailyAverages.length
+    };
+
+    const monthlyId = MonthlyAverages.insert(monthlyAverages);
+    MonthlyAverages.update({_id: {$in: _.pluck(monthlyAverages, "_id")}}, {
+        $set: { fkMonthly: monthlyId }
+    });
 }
--- imports/helpers/server/time-utils.js
+++ imports/helpers/server/time-utils.js
@@ -2,6 +2,22 @@ import { calculateHourlyAverage } from "/imports/helpers/server/hourly-utils.js"
 import { calculateDailyAverage } from "/imports/helpers/server/daily-utils.js";
 import { calculateMonthlyAverage } from "/imports/helpers/server/monthly-utils.js";
 
+export function getCurrentDay() {
+    return new Date().getDay();
+}
+
+export function getCurrentMonth() {
+    return new Date().getMonth();
+}
+
+export function getCurrentYear() {
+    return new Date().getYear();
+}
+
+export function getDaysInMonth(month, year = getCurrentYear()) {
+    return new Date(year, month, 0).getDate();
+}
+
 SyncedCron.add({
     name: "transferHourlyData",
     schedule: function(parser) {
